f0638721195599c560baa63cbb78f30f5e5f2557
david reinfurt <reinfurt@o-r-g.com>
2017-07-23 14:57:07 -0400

    add gyroscope logic from o-r-g

--- static/js/cube.js
+++ static/js/cube.js
@@ -1,14 +1,25 @@
+// globals
+
 var xAngle = 0, yAngle = 0;
+var cube = document.getElementById('cube');
 
-  
-document.addEventListener('click', function() { 
-    yAngle -= 180;
-    });
+// gyroscope 
+// chrome complains about this not being done of https
 
+window.addEventListener("deviceorientation", function(e) {        
+    var tilt = 0 - e.gamma;
+    device_orientation_handler(tilt);
+}, false);
+
+function device_orientation_handler(tilt) {
+    cube.style.transform = " rotateY("+ tilt +"deg)";
+    cube.style.webkitTransform  = " rotateY("+ tilt +"deg)";
+}
+
+// controls
 
 document.addEventListener('keydown', function(e) {
   switch(e.keyCode) {
-
     case 37: // left
       yAngle -= 90;
       break;
@@ -22,13 +33,12 @@ document.addEventListener('keydown', function(e) {
       xAngle -= 90;
       break;
     };
-
     document.getElementById('cube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 }, false);
 
-
 document.getElementById('control').addEventListener('click', function() { 
-    yAngle += 90;
-    document.getElementById('cube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+    device_orientation_handler(500);
+    // yAngle += 90;
+    // document.getElementById('cube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 }, false);
 
