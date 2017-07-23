22432e6e20b9ae7f3511b8081c966cfb83d038cb
david reinfurt <reinfurt@o-r-g.com>
2017-07-23 13:53:48 -0400

    v0.2.1

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 $uri = explode('/', $_SERVER['REQUEST_URI']);
-$view = "views/object.php";
+$view = "views/";
+
+if ($uri[1] == "cube") 
+    $view .= "cube.php";
+else 
+    $view .= "docket.php";
 
 require_once("views/head.php");
-// require_once("views/information.php");       // hardcoded
-// require_once("views/docket.php");            // open-rec-gen
-require_once("views/cube.php");
-// require_once($view);
+require_once($view);
 require_once("views/foot.php");
 ?>
