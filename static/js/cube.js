3f423f69063748e9364c89d312f8996e38e74661
david reinfurt <reinfurt@o-r-g.com>
2017-07-28 17:21:53 -0400

    add gyroscope rotate with limits

--- static/js/cube.js
+++ static/js/cube.js
@@ -2,7 +2,7 @@
 
 var xAngle = 0, yAngle = 0;
 var cube = document.getElementById('cube');
-
+var alpha, beta, gamma;     // gyrscope x, y, z (z is like y on iphone, points straight up)
 
 // event listeners
 
@@ -11,11 +11,12 @@ var cube = document.getElementById('cube');
 
 window.addEventListener("deviceorientation", function(e) {        
     var gain = 5.0;
-    var alpha = 0 - e.alpha * gain;
-    var beta = 0 - e.beta * gain;
-    var gamma = 0 - e.gamma * gain;
+    alpha = 0 - e.alpha * gain;
+    beta = 0 - e.beta * gain;
+    gamma = 0 - e.gamma * gain;
     // device_orientation_handler_xyz(alpha, beta, gamma);
-    device_orientation_handler_xyz(0, gamma, 0);
+    // device_orientation_handler_xyz(0, gamma, 0);
+    device_orientation_handler_limit(gamma);
 }, false);
 
 document.addEventListener('keydown', function(e) {
@@ -47,19 +48,26 @@ if (thiscontrol = document.getElementById('control')) {
 // display
 
 function device_orientation_handler(tilt) {
-/*
     cube.style.transform = " rotateX("+ tilt +"deg)";
     cube.style.webkitTransform  = " rotateX("+ tilt +"deg)";
-*/
     cube.style.transform = " rotateY("+ tilt +"deg)";
     cube.style.webkitTransform  = " rotateY("+ tilt +"deg)";
-/*
     cube.style.transform = " rotateZ("+ tilt +"deg)";
     cube.style.webkitTransform  = " rotateZ("+ tilt +"deg)";
-*/
 }
 
 function device_orientation_handler_xyz(alpha, beta, gamma) {
     cube.style.transform = " rotateX("+ alpha +"deg) rotateY("+ beta +"deg) rotateZ("+ gamma +"deg)";
     cube.style.webkitTransform = " rotateX("+ alpha +"deg) rotateY("+ beta +"deg) rotateZ("+ gamma +"deg)";
 }
+
+function device_orientation_handler_limit(gamma) {
+    if (gamma > 45) 
+        yAngle = 90;
+    else if (gamma < -45) 
+        yAngle = -90;
+    else 
+        yAngle = 0;
+    cube.style.transform = " rotateY("+ yAngle + "deg)";
+}
+
