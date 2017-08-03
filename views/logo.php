7cd2389306a1da00124a0e007e0ab3510d6ed98a
david reinfurt <reinfurt@o-r-g.com>
2017-08-03 10:21:38 +0100

    add hamburger logo

--- views/logo.php
+++ views/logo.php
@@ -1,7 +1,7 @@
+        <!-- <div id="logo-hamburger"><img class="r" src="/media/png/ica-ham-rgb.png"></div> -->
+        <!-- <div id="logo-hamburger"><img class="r" src="/media/png/ica-ham-rgb-spaced.png"></div> -->
+
         <div id="logo">
-            <!--
-            <div id="logo-r"><img class="r" src="media/png/ica-ham-rgb.png"></div>
-            -->
             <div id="logo-r"><img class="r" src="<? echo $host; ?>/media/png/logo-red.png"></div>
             <div id="logo-g"><img class="g" src="<? echo $host; ?>/media/png/logo-green.png"></div>
             <div id="logo-b"><img class="b" src="<? echo $host; ?>/media/png/logo-blue.png"></div>
